adee4983fae2320d8141de44728205652460d9b2
shivakrishna <shivakrishnagodugu@outlook.com>
2023-08-18 17:04:29 +0530

    discussion added

--- 1.Hello-world.sh
+++ 1.Hello-world.sh
@@ -2,8 +2,9 @@
 echo "Hello world" ,pulling to workspace
 
 P1=Shiva
-P2=Blaji
+P2=Balaji
+
 echo "$P1: Hi $P2, Good morning"
 echo "$P2: Hi $P1, Good morning"
-echo "$P1: How are $P2?"
+echo "$P1: How are you $P2?"
 echo "$P2: I'm good, How are you?"
