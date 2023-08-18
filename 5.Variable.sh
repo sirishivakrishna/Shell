f855840ab1e836c54467229385f1a8f27ffd87c4
shivakrishna <shivakrishnagodugu@outlook.com>
2023-08-18 17:42:24 +0530

    discussion added

--- 5.Variable.sh
+++ 5.Variable.sh
@@ -1,4 +1,8 @@
 
-echo "Please enter username:USERNAME"
+echo "Please enter username:"
 read -s USERNAME
 echo "User entered username as:$USERNAME"
+
+echo "Please enter password:"
+read -s PASSWORD
+echo "User entered username as:$PASSWORD"
