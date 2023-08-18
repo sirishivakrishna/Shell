1e9c4a6b6f28c4c79d8a159b51f5cca423f68e2d
shivakrishna <shivakrishnagodugu@outlook.com>
2023-08-18 17:12:32 +0530

    discussion added

--- 2.Variable.sh
+++ 2.Variable.sh
@@ -3,4 +3,4 @@
 # date command inside() will execute and pass it the result to variable calle DATE
 DATE=$(date)
 
-echo "The script execution timestamp is: $DATE
+echo "The script execution timestamp is:" $DATE
