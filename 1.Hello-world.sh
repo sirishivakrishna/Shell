93675f1602fd927198929dba64e7cbde9a709b6c
shivakrishna <shivakrishnagodugu@outlook.com>
2023-08-18 16:52:23 +0530

    discussion added

--- 1.Hello-world.sh
+++ 1.Hello-world.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "Hello world" ,pulling to workspace
+
+echo "Shiva: Hi Balaji, Good morning"
+echo "Balaji: Hi Shiva, Good morning"
+echo "Shiva: How are Blaji?"
+echo "Balaji: I'm good, How are you?"
