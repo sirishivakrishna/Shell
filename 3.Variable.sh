d8ae48fa8d98ac7c35c0339291071d7dc3968b1d
shivakrishna <shivakrishnagodugu@outlook.com>
2023-08-18 17:23:41 +0530

    discussion added

--- 3.Variable.sh
+++ 3.Variable.sh
@@ -1,5 +1,5 @@
-P1=$1
-P2=$2
+P1=$1  #this is fisrt value from the command line
+P2=$2  #this is second value from the command line
 
 echo "$P1: Hi $P2, Good morning"
 echo "$P2: Hi $P1, Good morning"
---
+++ 4.Variable.sh
@@ -0,0 +1,6 @@
+
+value1=$1
+value2=$2
+
+echo "$value1"
+echo "$value2"
