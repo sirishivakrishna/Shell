91fb48dab3120342d935876d0cb1cabf4d9b5511
shivakrishna <shivakrishnagodugu@outlook.com>
2023-08-19 17:39:28 +0530

    condition file1

--- 10.Specialvariables.sh
+++ 10.Specialvariables.sh
@@ -3,6 +3,8 @@ var2=$2
 var3=$3
 var4=$4
 
+
+echo "scriptname is: $0"
 echo "variable1 is: $1"
 echo "variable1 is: $2"
 echo "all variable1 is: $@"
--- 9.Functions.sh
+++ 9.Functions.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-user=$(id -u)
+date=$(date +%F)
+scriptname=$0
+Logfile=/temp/$scriptname-$date
 
+R=\e[31m
+G=\e[32m
+N=\e[0
+
+
+#this function should validate the previous command and inform user it is success or failure
 validate(){
-    if [ $1 -ne 0 ]
+    if [ $1 -ne 0 ] #It will receive the first argument
     then
-        echo "$2 ... Failure"
+        echo "$2 ...$R Failure $N"
         exit 1
     else
-        echo "$2 ... Success"
+        echo "$2 ... $F Success $N"
     fi
 }
 
-yum install git -y
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+    echo "Error :: Pleaase run this script with use"
+yum install git -y &>> Logfile
 validate $? "Instlling GIT"
 
-yum install mysql -y
+yum install mysql -y &>> Logfile
 validate $? "Instlling mysql"
 
-yum install postfix -y
+yum install postfix -y &>> Logfile
 validate $? "Instlling postfix"
