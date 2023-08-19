7b436d0eb636b231507f92c8c8346eb61c72805c
shivakrishna <shivakrishnagodugu@outlook.com>
2023-08-19 08:32:55 +0530

    condition file1

--- 7.Condition.sh
+++ 7.Condition.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 Number=$1
 
-IF[ $Number -gt 10 ]
-else
-echo "the numebr is greater than 10"
+IF [ $Number -gt 10 ]
 then
+echo "the numebr is greater than 10"
+else
 echo "the numebr is not greater than 10"
 fi
