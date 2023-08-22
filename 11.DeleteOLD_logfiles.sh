a15874d397790da5557acf0d2d189b84ffee4b32
shivakrishna <shivakrishnagodugu@outlook.com>
2023-08-22 08:30:20 +0530

    Commit1

--- 11.DeleteOLD_logfiles.sh
+++ 11.DeleteOLD_logfiles.sh
@@ -2,10 +2,10 @@
 
 App_Log_dir=/home/centos/app-logs
 
-date=$(date)
+DATE=$(date +%F %T)
 Logdir=/home/centos/Shelllogs
-Script_name=$0
-LogFile=$Logdir/$0-$date.log
+Script_name=$0 
+LogFile=$Logdir/$0-$DATE.log
 
 
 FILES_TO_DELETE=$(find $App_Log_dir -name "*.log" -type f -mtime +14)
@@ -14,6 +14,6 @@ echo "$FILES_TO_DELETE"
 
 while read line
 do
-    echo "Deleting $line" &>$LogFile
+    echo "Deleting $line" &>> $LogFile
     rm -rf $line
 done <<< $FILES_TO_DELETE
